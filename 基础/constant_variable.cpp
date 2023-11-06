05c7246f5f96d4fc2ebcce2c62ddb249fdf0b484
Lucasticc <lucasprint@163.com>
2023-11-06 15:55:01 +0800

    rename

--- 基础/constant_variable.cpp
+++ 基础/constant_variable.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 //常量的定义
+// 创建常量的时候必须指出相应数据类型 否则无法给变量分配内存
 #define day 7
 
 int main(){
@@ -15,6 +16,11 @@ int main(){
     const int month = 12;
     // month = 1 const修饰的变量也不能修改
     cout<<"一年一共"<< month <<"月"<< endl;
+
+    int num1 = 10;
+    int num2 = 20;
+    int sum = num1 + num2;
+    cout <<"sum="<<sum<< endl;
 
     return 0;
     
