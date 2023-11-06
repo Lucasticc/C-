fea64c48f9b42371a80f407883f014ccccd9872c
Lucasticc <lucasprint@163.com>
2023-11-06 15:09:14 +0800

    第一次提交

---
+++ c.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+cout<<"hello world" <<endl;
+cout<<"fre"<< endl;
+    system("pause");//暂停
+    /*达到
+    */
+    return 0;
+}
---
+++ x.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //定义一个变量
+    int a = 10;
+    cout<<"da" <<endl;
+    cout<<"a=" << a <<endl;
+    return 0;
+}
