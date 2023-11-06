606b41802ee3867745d7fc05aa61025a3b78b823
Lucasticc <lucasprint@163.com>
2023-11-06 15:30:36 +0800

    1

--- 基础/x.cpp
+++ 基础/x.cpp
@@ -1,13 +1,14 @@
 #include<iostream>
 using namespace std;
 //常量的定义
-#define wed = 7
+#define day 7
 
 int main(){
     //定义一个变量
     int a = 10;
     cout<<"da" <<endl;
     cout<<"a=" << a <<endl;
+    cout<<"一周一共"<< day <<"天"<< endl;
     return 0;
     
 }
