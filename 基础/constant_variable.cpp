fa5e1a1eb073130140e424696460e0c572356dab
Lucasticc <lucasprint@163.com>
2023-11-07 21:56:33 +0800

    rename

--- 基础/constant_variable.cpp
+++ 基础/constant_variable.cpp
@@ -3,6 +3,7 @@ using namespace std;
 //常量的定义
 // 创建常量的时候必须指出相应数据类型 否则无法给变量分配内存
 // 数据类型存在的原因是 在分配内存的时候根据 定义的数据类型来分配合适的空间
+// 使用sizeof 同志数据类型占用内存大小
 #define day 7
 
 int main(){
@@ -11,6 +12,10 @@ int main(){
     cout<<"da" <<endl;
     cout<<"a=" << a <<endl;
     cout<<"一周一共"<< day <<"天"<< endl;
+    //整形 short（2） int（4） long（4） longlong（8）在win下long 为4 在linux long为8
+    int s1  = 10;
+    cout <<"占用内存空间：" << sizeof(s1) << endl;
+    cout <<"long 占用内存空间：" << sizeof(long long) << endl;
 
 
     // int month = 12;
